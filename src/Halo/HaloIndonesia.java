f0a06fb9b70e48a968c233314a1214fe888f24f9
naufalmuzakkir444-arch <naufalmuzakkir444@gmail.com>
2025-09-14 06:30:50 +0800

    Commit Pertama

---
+++ src/Halo/HaloIndonesia.java
@@ -0,0 +1,18 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Halo;
+
+/**
+ *
+ * @author USER MSI
+ */
+public class HaloIndonesia {
+    public static void main(String[] args) {
+        System.out.println("Halo Indonesia");
+        System.out.println("Selamat Pagi Indonesia");
+        System.out.println("Jaya Indonesia");
+                
+    }
+}
---
+++ src/oop/concept/OOPConcept.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package oop.concept;
+
+/**
+ *
+ * @author USER MSI
+ */
+public class OOPConcept {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
